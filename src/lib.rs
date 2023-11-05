8ece733f781f6dc931d11eed4080fd60ec683847
nan mu <mu.nan.11@outlook.com>
2023-11-05 14:52:47 +0800

    cut two unstable lint dny

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,20 @@
+#![no_std]
+
 pub mod svd;
+
+#[no_mangle]
+static mut DEVICE_PERIPHERALS: bool = false;
+
+impl svd::Peripherals {
+    #[doc = r"Returns all the peripherals *once*"]
+    #[inline]
+    pub fn take() -> Option<Self> {
+        cortex_m::interrupt::free(|_| {
+            if unsafe { DEVICE_PERIPHERALS } {
+                None
+            } else {
+                Some(unsafe { svd::Peripherals::steal() })
+            }
+        })
+    }
+}
